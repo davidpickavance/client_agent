2a582c261a83e4ea54cf1f0fe391b1bbf49253ec
davidpickavance <davidpickavance@gmail.com>
2015-11-04 16:42:54 -0800

    Updated Readme

--- README.md
+++ README.md
@@ -1,43 +1,17 @@
-# Welcome to the Python Guild!
+# Transferring a call with Twilio
 
-As members of the Python guild, you will be working through the challenges of TwilioQuest using the Python programming language.  This project is pre-configured to do some interesting Twilio stuff using Python and the [Flask](http://flask.pocoo.org/) web framework.
+This code is an example of how to transfer a call using Twilio. It has been written for the
+following very specific scenario only:
 
-## Setting Up
+An agent, using Twilio Client calls a customer. The agent wants to transfer the customer
+to a colleague so presses the "Transfer" button on the Client. This puts the customer
+on hold and connects the agent to the colleague. After a brief chat, the colleague agrees
+to talk to the customer so the agent presses "Transfer" again. The agent, customer and
+colleague are all, now, in a three way call. The agent completes the transfer by hanging
+up.
 
-We assume that before you begin, you will have [Python](http://www.python.org/) and [pip](http://www.pip-installer.org/en/latest/) installed on your system and available at the command line.
+Over time, I plan to increase the complexity, and real world applicability, of this demo
+by allowing for multiple client connections and multiple concurrent calls. I'm sure my
+plans will solidify as I work on them. 
 
-Before you can run this project, you will need to set three system environment variables.  These are:
 
-* `TWILIO_ACCOUNT_SID` : Your Twilio "account SID" - it's like your username for the Twilio API.  This and the auth token (below) can be found [on your account dashboard](https://www.twilio.com/user/account).
-* `TWILIO_AUTH_TOKEN` : Your Twilio "auth token" - it's your password for the Twilio API.  This and the account SID (above) can be found [on your account dashboard](https://www.twilio.com/user/account).
-* `TWILIO_NUMBER` : A Twilio number that you own, that can be used for making calls and sending messages.  You can find a list of phone numbers you control (and buy another one, if necessary) [in the account portal](https://www.twilio.com/user/account/phone-numbers/incoming).
-
-For Mac and Linux, environment variables can be set by opening a terminal window and typing the following three commands - replace all the characters after the `=` with values from your Twilio account:
-
-    export TWILIO_ACCOUNT_SID=ACXXXXXXXXX
-    export TWILIO_AUTH_TOKEN=XXXXXXXXX
-    export TWILIO_NUMBER=+16518675309
-
-To make these changes persist for every new terminal (on OS X), you can edit the file `~/.bash_profile` to contain the three commands above.  This will set these environment variables for every subsequent session. Once you have edited the file to contain these commands, run `source ~/.bash_profile` in the terminal to set up these variables.
-
-On Windows, the easiest way to set permanent environment variables (as of Windows 8) is using the `setx` command.  Note that there is no `=`, just the key and value separated by a space:
-
-    setx TWILIO_ACCOUNT_SID ACXXXXXXXXX
-    setx TWILIO_AUTH_TOKEN XXXXXXXXX
-    setx TWILIO_NUMBER +16518675309
-
-## Running the application
-
-[Download the project source code directly](https://github.com/twilio/starter-python/archive/master.zip) or [clone the repository on GitHub](https://github.com/twilio/starter-python).  Navigate to the folder with the source code on your machine in a terminal window.
-
-You will first need to install the application's dependencies. To install them  manually, type the following commands in your terminal:
-
-    pip install twilio
-    pip install flask
-
-Now, you should be able to launch the application.  From your terminal, run `python app.py`.  This should launch your sinatra application on port 5000 - [visit that URL on your local host](http://localhost:5000/).  Enter your mobile number in the fields provided, and test both SMS text messages and phone calls being sent to the mobile number you provide.  The web UI should look something like this:
-
-![python guild](http://demo.kevinwhinnery.com/upload/Welcome_to_the_Python_Guild%21-20130827-150203.png)
-
-## Begin Questing!
-This is but your first step into a larger world.  [Return to TwilioQuest](http://quest.twilio.com) to continue your adventure.  Huzzah!
--- app.py
+++ app.py
@@ -9,107 +9,131 @@ from twilio.rest import TwilioRestClient
 from twilio.util import TwilioCapability
 from twilio.access_token import AccessToken
 
-# Pull in configuration from system environment variables
-TWILIO_ACCOUNT_SID = "AC662af4948fd5a88cbfc48a44a11b0898"
-TWILIO_AUTH_TOKEN = "058d7ef7a73e65c526122a5fd9951a4d"
-TWILIO_NUMBER = "+14156884963"
+# Required to connect to Twilio
+TWILIO_ACCOUNT_SID = "AC662af494000000000000000000000000"
+TWILIO_AUTH_TOKEN = "058d0000000000000000000000000000"
+TWILIO_NUMBER = "+14155554963"
 
 # create an authenticated client that can make requests to Twilio for your
 # account.
 client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+# Allows us to keep a track of how many times the agent has pressed the
+# "transfer" button for this call
 xferd = []
 
 # Create a Flask web app
 app = Flask(__name__)
 
-# Render the home page
+# Render the Twilio Client as a web page
 @app.route('/')
 def index():
+    # Create a capability token for this client instance
     capability = TwilioCapability(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-    capability.allow_client_outgoing("AP17a745dc5cfc4c9dbcc0886594fd1471")
+    # If the client attempts an outgoing call, invoke a TwiML app
+    capability.allow_client_outgoing("AP17a745dc5cfc00000000000000000000")
+    # We can make incoming calls to this client with <dial><client>david</client></dial>
     capability.allow_client_incoming("david")
     token = capability.generate()
-    print token
     return render_template("client_browser.html",
                            capability_token=token)
 
-# Handle a POST request to send a text message. This is called via ajax
-# on our web page
-@app.route('/message', methods=['POST'])
-def message():
-    # Send a text message to the number provided
-    message = client.sms.messages.create(to=request.form['to'],
-                                         from_=TWILIO_NUMBER,
-                                         body='Good luck on your Twilio quest!')
 
-    # Return a message indicating the text message is enroute
-    return 'Message on the way!'
-
-# Handle a POST request to make an outbound call. This is called via ajax
-# on our web page
-@app.route('/call', methods=['POST'])
-def call():
-    # Make an outbound call to the provided number from your Twilio number
-    call = client.calls.create(to=request.form['to'], from_=TWILIO_NUMBER, 
-                               url='http://twimlets.com/message?Message%5B0%5D=http://demo.kevinwhinnery.com/audio/zelda.mp3')
-
-    # Return a message indicating the call is coming
-    return 'Call inbound!'
-
-# Generate TwiML instructions for an outbound call
-@app.route('/hello')
-def hello():
-    response = twiml.Response()
-    response.say('Hello there! You have successfully configured a web hook.')
-    response.say('Good luck on your Twilio quest!', voice='woman')
-    return Response(str(response), mimetype='text/xml')
-
+# The Twilio Client has attempted to make an outbound call
+# This function makes that call happen
 @app.route('/client_call', methods=['GET', 'POST'])
 def client_pstn():
-    callerID = "+14156884963"
+    # because we are calling out to the PSTN, we need to provide a phone
+    # number, associated with the same Twilio account as the client, as
+    # caller ID. It needs to be in e.164 format
+    callerID = "+14155554963"
+    # Get the dialed number from the parameters in the HTTP request
     called_number = request.values.get('tocall')
+    # Generate a TwiML response...
     response = twiml.Response()
+    # ...and actually place a call to the dialed number
+    # when the client initiates a transfer and puts this call
+    # on hold, the action url will be invoked, giving us an
+    # opportunity to update the Client leg
     with response.dial(callerId=callerID, action="/agent_into_conf") as r:
         r.number(called_number)
     return str(response)
 
+
+# Ooh, the person using the Twilio Client has pressed the "Transfer" button
+# This allows them to start the process of transferring the called party to
+# a colleague (for example)
 @app.route('/transfer', methods=['POST', 'GET'])
 def transfer():
     
+    # The Twilio Client has told us what the call sid is for it's connection
+    # to Twilio. We want to find out what the call sid is for the called
+    # party's connection to Twilio. This will be listed as a child call on
+    # the Client's call
     call_sid = request.values.get('params[CallSid]')
     child_calls = client.calls.list(parent_call_sid=call_sid)
-    if call_sid in xferd:
-        for child_call in child_calls:
-            client.calls.update(child_call.sid, method="POST",
-                                url="http://2deedf27.ngrok.io/customer_to_conf")
-    else:
-
-        second_agent_call = client.calls.create(to="+15103320285", from_="+15102543413",
-                                                url="http://2deedf27.ngrok.io/second_agent_in_conf")
-    #for key, value in request.values.iteritems():
-    #    print "%s is %s" % (key, value)
     
-    #call = client.calls.get(call_sid)
-        
-    #print dir(call)
+    # The transfer button has two similar but different functions
+    # 1) start the transfer process by putting the called party on hold
+    #    and initiating a call to the colleague
+    # 2) take the called party off hold and connect them to the 
+    #    colleague
+    # The easy way of figuring out which the person using the Client
+    # wants to do is by figuring out whether they have already pressed
+    # the Transfer button on this call. If they have not, they want to
+    # do (1). If this is the second time they have pressed the button on
+    # this call, they want to do (2).
+    if call_sid not in xferd:
+        # For ease, the second agent, the colleague, is on a cell phone. Again,
+        # we provide a url so that we can provide TwiML to control this new call
+        # leg
+        second_agent_call = client.calls.create(to="+15105550555", from_="+15105553413",
+                                                url="http://2deedf27.ngrok.io/second_agent_in_conf")
         for child_call in child_calls:
-        #print dir(child_call)
             print call_sid
             print child_call.sid
+            # To place the called party on hold, we make a REST request
+            # to update their call leg (identified via call sid) and 
+            # Twilio will then send a request to the url specified
+            # so that we can provide instructions about how to update
+            # the call leg via TwiML
             client.calls.update(child_call.sid, method="POST",
                                 url="http://2deedf27.ngrok.io/called_on_hold")
             xferd.append(call_sid)
+    else:
+        for child_call in child_calls:
+            # this is the second time the transfer button has been pressed
+            # for this call so we will put the called party into the 
+            # conference with the colleague
+            client.calls.update(child_call.sid, method="POST",
+                                url="http://2deedf27.ngrok.io/customer_to_conf")
     return "hello"
 
+
+# This is the URL we gave to Twilio when we asked to update
+# the call leg of the called party at the start of the transfer
+# process. We will use TwiML to put the called party into a 
+# conference that contains only them and some hold music
 @app.route('/called_on_hold', methods=['POST', 'GET'])
 def called_on_hold():
     print request.values
     response = twiml.Response()
     with response.dial() as r:
+        # A nice, simple conference name. I can do this here
+        # because I only write this web app to handle one Client
+        # and that Client can only have one call going at a time
+        # To make this web app more general, we would need to 
+        # come up with a process for giving each "Hold" conference
+        # a unique but meaningful name, so that we know how to 
+        # reference it later
         conference_name = "Hold"
         r.conference(conference_name, endConferenceOnExit=True)
     return str(response)
 
+
+# Either the called party hung up before we managed to transfer
+# them or we just put them on hold. Here, we will figure out
+# which
 @app.route('/agent_into_conf', methods=['POST', 'GET'])
 def agent_into_conf():
     print "in agent into conf"
@@ -117,14 +141,21 @@ def agent_into_conf():
     child_sid = request.values.get("DialCallSid")
     child_call = client.calls.get(child_sid)
     response = twiml.Response()
+    # if the customer hasn't hung up, it means we've
+    # put them on hold so we should update the Client
+    # call leg to put them into a conference with the
+    # colleague
     if child_call.status == "in-progress": 
         with response.dial() as r:
             conference_name = "xfer"
             r.conference(conference_name)
+    # if they have hung up, we should follow suit
     else:
         response.hangup()
     return str(response)
 
+# we'll put our colleague into a converence so that they can
+# await our arrival
 @app.route("/second_agent_in_conf", methods=["POST", "GET"])
 def second_agent_in_conf():
     response = twiml.Response()
@@ -133,6 +164,7 @@ def second_agent_in_conf():
         r.conference(conference_name)
     return str(response)
 
+# Put the customer into the conference with our colleague
 @app.route("/customer_to_conf", methods=['POST','GET'])
 def customer_to_conf():
     response=twiml.Response()
